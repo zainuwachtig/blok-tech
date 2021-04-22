0ff224dd7d129670f8cdd608c2e23047f062a976
zainuwachtig <zain.butt@hva.nl>
2021-04-22 12:34:14 +0200

    Static files

--- server.js
+++ server.js
@@ -2,9 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//Waarom moet deze als eerst?
+app.use(express.static('static'))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/about', function (req, res) {
+  res.send('About this application')
+})
+
+app.get('/login', function (req, res) {
+  res.send('Login page')
+})
+
+app.use(function (req, res) {
+  res.status(404).send("Sorry, not found")
+})
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
