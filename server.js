e53a0ddc5e826a3a20307da0c5720cf1f48c5ca6
zainuwachtig <zain.butt@hva.nl>
2021-04-24 18:26:56 +0200

    Pug toegevoegd

--- server.js
+++ server.js
@@ -4,15 +4,29 @@ const port = 3000
 
 app.set('view engine', 'pug')
 
-// Als ik deze doe dan werkt het vanaf line 10 meer
-//app.use(express.static('static'))
-
-app.get('/static', (req, res) => {
+// Ik snap niet waarom deze bovenaan moet, is het omdat die boven line 23 moet zijn met de 404?
+app.get('/', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
+
+app.use(express.static('static'))
+
+app.get('/about', function (req, res) {
+  res.send('About this application')
+})
+
+app.get('/login', function (req, res) {
+  res.send('Login page')
+})
+
+app.use(function (req, res) {
+  res.status(404).send("Sorry, not found")
+})
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-
-
