62f322a6d03c117f86624b9c577e0d64bae4f8ee
zainuwachtig <zain.butt@hva.nl>
2021-05-12 15:04:22 +0200

    Form gefixt om model te kiezen

--- server.js
+++ server.js
@@ -3,29 +3,9 @@ const app = express()
 const port = 3000
 const StockXAPI = require('stockx-api');
 const stockX = new StockXAPI();
-
-const shoesArray = [{
-  'name': 'Air Max 95 OG Neon (2020)',
-  'releaseDate': '2020',
-  'pid': 'CT1689-001',
-  'image': 'https://images.stockx.com/images/Nike-Air-Max-95-OG-Neon-2020-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&trim=color&q=90&dpr=2&updated_at=1609443450',
-  'urlKey': 'air-max-95-og-neon-2020'
-},
-{
-  'name': 'Air Max 98 Supreme Snakeskin',
-  'releaseDate': '2016',
-  'pid': '844694-100',
-  'image': 'https://images.stockx.com/images/Nike-Air-Max-98-Supreme-Snakeskin-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&trim=color&q=90&dpr=2&updated_at=1607672309',
-  'urlKey': 'air-max-98-supreme-snakeskin'
-},
-{
-  'name': 'Air Max 180 Comme des Garcons White',
-  'releaseDate': '2018',
-  'pid': 'AO4641-600',
-  'image': 'https://images.stockx.com/Nike-Air-Max-180-Comme-des-Garcons-White-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&trim=color&q=90&dpr=2&updated_at=1603481985',
-  'urlKey': 'air-max-180-comme-des-garcons-white'
-}
-];
+// Deze aanvullen met zoveel mogelijk modellen
+const allModels = ['Air Max 1', 'Air Max 90', 'Air Max 95', 'Air Max 96', 'Air Max 97', 'Air Max 98', 'Air Max 270', 'Air Max 720', 'Air Max Plus', 'Air VaporMax'];
+let yourModels = [];
 
 app.set('view engine', 'pug')
 
@@ -43,17 +23,22 @@ app.get('/login', (req, res) => {
   res.render('login')
 })
 
+app.get('/model', (req, res) => {
+  res.render('model', {allModels})
+})
+
+// Kan er geen limit op te zetten ivm laden?
+// math.random nog fixen zodat die niet de hele tijd de eerste uit de array pakt.
 app.get('/shoe/:urlKey', async (req, res) => {
-  // const shoe = shoesArray.find(shoe => shoe.urlKey == req.params.urlKey)
-  let shoe = await stockX.searchProducts('air max 95', {
+  let shoe = await stockX.searchProducts('air max 95 dusty purple', {
     limit: 1
   })
   .then(products => products)
   .catch(err => console.log(`Error searching: ${err.message}`));
   res.render('shoe', {shoe})
+  // Hij herkent shoe wel maar niet de urlKey?
   console.log(shoe)
 })
-
 
 
 app.use( (req, res) => {
