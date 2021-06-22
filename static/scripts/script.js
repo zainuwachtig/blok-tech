0cc5269ee0c6e3bd6d881e6f4aecb149864a907b
zainuwachtig <zain.butt@hva.nl>
2021-06-22 11:59:25 +0200

    feedback message toegevoegd

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,19 +1,49 @@
 const superlike = document.getElementsByClassName("fa-star");
 const trash = document.getElementsByClassName("fa-trash");
 const copy = document.getElementsByClassName("fa-copy");
+const myLikesMessage = document.getElementById("mylikesmessage");
+const likedShoes = document.getElementsByClassName("mylikescardpaartjes");
 
 function toggleSuperLike() {
     this.parentNode.parentNode.classList.toggle("addsuperlike")
-    this.classList.toggle("superliked")
+    this.classList.toggle("superliked");
+    if (this.classList.contains("superliked")) {
+        createMessage('Superliked!')
+    } else {
+        createMessage('Un-superliked!')
+    }
+    deleteMessage();
 }
 
 function removeShoe() {
     this.parentNode.parentNode.remove();
+    createMessage('Deleted the shoe :(')
+    deleteMessage();
+    
 }
 
 function copyStyle() {
     // Dit had denk ik wel mooier gekund, maar het gaat erom dat het werkt.
-    navigator.clipboard.writeText(this.parentNode.parentNode.childNodes[2].childNodes[1].textContent)
+    // https://developer.mozilla.org/en-US/docs/Web/API/Clipboard_API
+    navigator.clipboard.writeText(this.parentNode.parentNode.childNodes[2].childNodes[1].textContent);
+    createMessage('Copied the stylecode to clipboard!')
+    deleteMessage();
+}
+
+function createMessage(message) {
+    var newMessage = document.createElement("p");  
+    var messageContent = document.createTextNode(message); 
+    newMessage.appendChild(messageContent); 
+    newMessage.setAttribute("id", "mylikesmessage");
+    likedShoes[0].parentNode.appendChild(newMessage); 
+    
+}
+
+function deleteMessage() {
+    window.setTimeout(function() {
+        const myLikesMessage = document.getElementById("mylikesmessage");
+        myLikesMessage.remove()
+     }, 10000);
 }
 
 // Een for loop, omdat die anders niet werkt voor alle superlikes.
