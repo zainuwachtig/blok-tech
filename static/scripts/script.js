645959c5b94d89cbdaa0692d115f2bd61fd6c244
zainuwachtig <zain.butt@hva.nl>
2021-06-23 21:27:31 +0200

    Linter op de script.js

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,62 +1,59 @@
-const superlike = document.getElementsByClassName("fa-star");
-const trash = document.getElementsByClassName("fa-trash");
-const copy = document.getElementsByClassName("fa-copy");
-const myLikesMessage = document.getElementById("mylikesmessage");
-const likedShoes = document.getElementsByClassName("mylikescardpaartjes");
-
-function toggleSuperLike() {
-    this.parentNode.parentNode.classList.toggle("addsuperlike")
-    this.classList.toggle("superliked");
-    if (this.classList.contains("superliked")) {
-        createMessage('Superliked!')
-    } else {
-        createMessage('Un-superliked!')
-    }
-    deleteMessage();
-}
-
-function removeShoe() {
-    this.parentNode.parentNode.remove();
-    createMessage('Deleted the shoe :(')
-    deleteMessage();
-    
-}
-
-function copyStyle() {
-    // Dit had denk ik wel mooier gekund, maar het gaat erom dat het werkt.
-    // https://developer.mozilla.org/en-US/docs/Web/API/Clipboard_API
-    navigator.clipboard.writeText(this.parentNode.parentNode.childNodes[2].childNodes[1].textContent);
-    createMessage('Copied the stylecode to clipboard!')
-    deleteMessage();
-}
+const superlike = document.getElementsByClassName('fa-star');
+const trash = document.getElementsByClassName('fa-trash');
+const copy = document.getElementsByClassName('fa-copy');
+const likedShoes = document.getElementsByClassName('mylikescardpaartjes');
 
 function createMessage(message) {
-    var newMessage = document.createElement("p");  
-    var messageContent = document.createTextNode(message); 
-    newMessage.appendChild(messageContent); 
-    newMessage.setAttribute("id", "mylikesmessage");
-    likedShoes[0].parentNode.appendChild(newMessage); 
-    
+  const newMessage = document.createElement('p');
+  const messageContent = document.createTextNode(message);
+  newMessage.appendChild(messageContent);
+  newMessage.setAttribute('id', 'mylikesmessage');
+  likedShoes[0].parentNode.appendChild(newMessage);
 }
 
 function deleteMessage() {
-    window.setTimeout(function() {
-        const myLikesMessage = document.getElementById("mylikesmessage");
-        myLikesMessage.remove()
-     }, 2500);
+  window.setTimeout(function () {
+    const myLikesMessage = document.getElementById('mylikesmessage');
+    myLikesMessage.remove();
+  }, 2500);
 }
 
-// Een for loop, omdat die anders niet werkt voor alle superlikes.
-for (var i = 0; i < superlike.length; i++) {
-    superlike[i].addEventListener("click" , toggleSuperLike) ; 
- }
+function toggleSuperLike() {
+  this.parentNode.parentNode.classList.toggle('addsuperlike');
+  this.classList.toggle('superliked');
+  if (this.classList.contains('superliked')) {
+    createMessage('Superliked!');
+  } else {
+    createMessage('Un-superliked!');
+  }
+  deleteMessage();
+}
 
-// Een for loop, omdat die anders niet werkt voor alle prullebakken.
-for (var i = 0; i < trash.length; i++) {
-    trash[i].addEventListener("click" , removeShoe) ; 
- }
+function removeShoe() {
+  // createMessage voor de .remove(), omdat die anders het parent element niet herkend
+  createMessage('Deleted the shoe :(');
+  this.parentNode.parentNode.remove();
+  deleteMessage();
+}
 
- // Een for loop, omdat die anders niet werkt voor alle copy buttons.
-for (var i = 0; i < copy.length; i++) {
-    copy[i].addEventListener("click" , copyStyle) ; 
- }
+function copyStyle() {
+  // https://developer.mozilla.org/en-US/docs/Web/API/Clipboard_API
+  navigator.clipboard.writeText(
+    this.parentNode.parentNode.childNodes[2].childNodes[1].textContent
+  );
+  createMessage('Copied the stylecode to clipboard!');
+  deleteMessage();
+}
+
+// For loops gebruikt, omdat ze anders niet voor alle icons (superlike, trash en copy) gelden
+for (let i = 0; i < superlike.length; i += 1) {
+  superlike[i].addEventListener('click', toggleSuperLike);
+}
+
+for (let i = 0; i < trash.length; i += 1) {
+  trash[i].addEventListener('click', removeShoe);
+}
+
+for (let i = 0; i < copy.length; i += 1) {
+  copy[i].addEventListener('click', copyStyle);
+}
